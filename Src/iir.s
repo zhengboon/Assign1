d3968ccf16b5f56d948a481893eabd9d9030b0ff
zhengboon <zhengboon1@gmail.com>
2025-09-14 08:42:39 +0800

    a

--- Src/iir.s
+++ Src/iir.s
@@ -44,11 +44,39 @@ iir:
 
 SUBROUTINE:
 PUSH {R4-R11, LR}
-MOV R4,R0
-MOV R5,R1
-MOV R6,R2
-MOV R7,R3
+MOV R8,R0 	 @	int N = 4;//first parameter
+MOV R9,R1  	 @	int b[N_MAX+1] = {100, 250, 360, 450, 580}; //N+1 dimensional feedforward
+MOV R10,R2 	 @	int a[N_MAX+1] = {100, 120, 180, 230, 250}; //N+1 dimensional feedback
+MOV R11,R3 	 @	int x_n
+MOV R1,#0    @	static int x_store[N_MAX] = {0}; // to store the previous N values of x_n.
+MOV R2,#0    @	int j;
+MOV R3,#0    @	static int y_store[N_MAX] = {0}; // to store the previous values of y_n.
+@	y_n = x_n*b[0]/a[0];
+LDR R0,[R9,R2,LSL#2]
+LDR R4,[R10,R2,LSL#2]
+MUL R0, R0, R11
+SDIV R0, R0,R4
 
+
+
+
+@	for (j=0; j<N; j++)
+@	{
+@		y_n+=(b[j+1]*x_store[j]-a[j+1]*y_store[j])/a[0];//pain
+@	}
+@
+@	for (j=N-1; j>0; j--)
+@	{
+@		x_store[j] = x_store[j-1];
+@		y_store[j] = y_store[j-1];
+@	}
+
+@	x_store[0] = x_n;
+@	y_store[0] = y_n;
+@
+@	y_n /= 100; // scaling down
+
+@	return y_n;
 
 
 
